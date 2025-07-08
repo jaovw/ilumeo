9e800f4e34aecbd88d3ad8de576e944efc674711
joaowonccepro <joao.woncce@prodigitalhr.com.br>
2025-07-07 22:42:41 -0300

    ilumeo: - removing default content and css - add iframe figma for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main style={{ display: 'flex', justifyContent: 'center', padding: '2rem' }}>
+      <iframe
+        style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/fQaTM68I4Bi8YnmFzoTNFk/Ilumeo---Teste-Fullstack?node-id=26%3A2"
+        allowFullScreen
+      ></iframe>
+    </main>
   )
 }
 
